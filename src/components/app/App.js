9523fe497fc4dbba10a9e78620b04594d462d991
Evgeniya <74735099+sanny410@users.noreply.github.com>
2022-04-12 18:47:22 +0300

    deploy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, { useEffect, useState} from "react";
 import SortUser from "../sortUser/sortUser";
 import MainPage from "../mainPage/mainPage";
 
